a40fa8152916832d2e35eb2c0728265042fd895c
NathanielWroblewski <nathanielwroblewski@gmail.com>
2025-07-04 12:51:31 -0400

    Add waybar system tray

--- pkgsets/desktop.nix
+++ pkgsets/desktop.nix
@@ -7,6 +7,7 @@
         niri # wayland compositor, tiling window manager
         greetd.tuigreet # display manager and login, optional
         brightnessctl # control screen brightness
+        waybar # system tray / bar
         rofi # application launcher and window switcher
         nautilus # file explorer
         zsh # shell
