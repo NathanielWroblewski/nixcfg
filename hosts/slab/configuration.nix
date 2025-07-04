f714f4913743e4aef66f921334ba04bbd1cab6b8
NathanielWroblewski <nathanielwroblewski@gmail.com>
2025-07-04 13:22:01 -0400

    Add waybar config

--- hosts/slab/configuration.nix
+++ hosts/slab/configuration.nix
@@ -45,6 +45,7 @@ in
       ../../pkgcfg/system-level/1pass.nix # password manager
       ../../pkgcfg/system-level/niri.nix # wayland compositor, tiling window manager
       ../../pkgcfg/system-level/tuigreet.nix # display manager and login
+      ../../pkgcfg/system-level/waybar.nix
       ../../pkgcfg/system-level/zsh.nix # shell
       # ../../pkgcfg/system-level/docker.nix
       # ../../pkgcfg/system-level/hyprland.nix
---
+++ pkgcfg/system-level/waybar.nix
@@ -0,0 +1,63 @@
+{
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+
+    settings = [
+      {
+        layer = "top";
+        height = 23;
+        margin = 0;
+        spacing = 10;
+        border-size = 2;
+        padding = 10;
+        position = "top";
+
+        modules-left = [
+          "custom/files"
+        ];
+        modules-center = [ ];
+        modules-right = [
+          "battery"
+          "clock"
+        ];
+
+        battery = {
+          format = "{capacity}% {icon}";
+          format-alt = "{time} {icon}";
+          format-charging = "{capcity}% 🔋";
+          format-plugged = "{capacity}% 🔌";
+
+          states = {
+            critical = 15;
+            warning = 30;
+          };
+        };
+
+        clock = {
+          format-alt = "{:%Y-%m-%d}";
+          tooltip-format = "{:%Y-%m-%d | %H:%M}";
+        };
+
+        network = {
+          interval = 1;
+          format-disconnected = "";
+        };
+
+        "custom/files" = {
+          exec = "echo 'File'";
+          interval = 60;
+          return-type = "plain";
+          on-click = "nautilus";
+        };
+
+        "custom/help" = {
+          exec = "echo 'Help'";
+          interval = 60;
+          return-type = "plain";
+          on-click = "niri msg action show-hotkey-overlay";
+        };
+      }
+    ];
+  };
+}
