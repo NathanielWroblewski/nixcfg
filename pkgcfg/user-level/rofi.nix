3f003cbcf13aa586aa6bcd236fee7345d3f56251
NathanielWroblewski <nathanielwroblewski@gmail.com>
2025-07-03 21:12:08 -0400

    wip rofi config

---
+++ pkgcfg/user-level/rofi.nix
@@ -0,0 +1,17 @@
+{ constants, ... }:
+let
+  fonts = constants.themes.fonts;
+in
+{
+  programs.rofi = {
+    enable = true;
+
+    font = fonts.mono;
+
+    # terminal = path to alacritty
+    # theme = ...
+    # configPath
+    # modes = [];
+    # extraConfig = keybindings...
+  };
+}
