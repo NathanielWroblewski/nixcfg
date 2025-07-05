cdd641f97666dfef1924bf89126ff807100d3fbd
NathanielWroblewski <nathanielwroblewski@gmail.com>
2025-07-05 11:39:32 -0400

    Drop swaylock

--- pkgsets/wayland.nix
+++ pkgsets/wayland.nix
@@ -7,8 +7,7 @@
     brightnessctl # control screen brightness
     waybar # system tray / bar
     swayidle # wayland idle manager
-    swaylock # wayland screen locking utility
-    swaylock-effects # more aesthetic and functional screenlocker
+    swaylock-effects # wayland screen locking utility
     rofi # application launcher and window switcher
     nautilus # file explorer
   ];
