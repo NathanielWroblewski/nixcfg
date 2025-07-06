73871509a0b293e61ed2e347f4e526d26e7c44de
NathanielWroblewski <nathanielwroblewski@gmail.com>
2025-07-05 23:16:34 -0400

    Use colors from active theme in swaylock styles

--- pkgcfg/user-level/swaylock-effects.nix
+++ pkgcfg/user-level/swaylock-effects.nix
@@ -1,42 +1,44 @@
 { pkgs, lib, constants, ... }:
 let
   trim = lib.strings.trim;
-  themeFonts = constants.themes.fonts;
+  fonts = constants.themes.fonts;
+  colors = constants.themes.colors.active;
+  users = constants.users;
 in
 {
   cmd = trim ''
     ${pkgs.swaylock-effects}/bin/swaylock -f \
-      --bs-hl-color ffffffff \
+      --bs-hl-color ${colors.primary.foreground} \
       --clock \
       --daemonize \
       --effect-blur 15x8 \
       --effect-vignette 0.3:0.3 \
       --fade-in 1 \
-      --font "${themeFonts.sans}" \
+      --font "${fonts.sans}" \
       --ignore-empty-password \
-      --image /home/nathaniel/Pictures/.background-image.jpg \
+      --image /home/${users.nathaniel}/Pictures/.background-image.jpg \
       --indicator \
       --indicator-caps-lock \
       --indicator-idle-visible \
       --indicator-radius 120 \
       --indicator-thickness 7 \
-      --inside-clear-color 34c75940 \
-      --inside-color 00000040 \
-      --inside-ver-color 00000020 \
-      --inside-wrong-color ff3b3040 \
-      --key-hl-color ffffffff \
+      --inside-clear-color 00000000 \
+      --inside-color 00000000 \
+      --inside-ver-color 00000000 \
+      --inside-wrong-color ${colors.normal.red} \
+      --key-hl-color ${colors.primary.foreground} \
       --line-clear-color 00000000 \
       --line-color 00000000 \
       --line-wrong-color 00000000 \
-      --ring-clear-color 34c759 \
-      --ring-color d0d0d0 \
-      --ring-ver-color ffffff \
-      --ring-wrong-color ff3b30 \
+      --ring-clear-color ${colors.primary.foreground} \
+      --ring-color ${colors.primary.foreground} \
+      --ring-ver-color ${colors.primary.foreground} \
+      --ring-wrong-color ${colors.normal.red} \
       --separator-color 00000000 \
       --show-failed-attempts \
-      --text-clear-color ffffff \
-      --text-color ffffff \
-      --text-ver-color ffffff \
-      --text-wrong-color ffffff      
+      --text-clear-color ${colors.primary.foreground} \
+      --text-color ${colors.primary.foreground} \
+      --text-ver-color ${colors.primary.foreground} \
+      --text-wrong-color ${colors.primary.foreground}      
   '';
 }
