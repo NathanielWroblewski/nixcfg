f7ccdea3965c847e78371d02267a6caae3807c14
NathanielWroblewski <nathanielwroblewski@gmail.com>
2025-07-05 22:41:33 -0400

    Add more swaylock configuration

--- pkgcfg/user-level/swaylock-effects.nix
+++ pkgcfg/user-level/swaylock-effects.nix
@@ -6,32 +6,37 @@ in
 {
   cmd = trim ''
     ${pkgs.swaylock-effects}/bin/swaylock -f \
-      --image /home/nathaniel/Pictures/.background-image.jpg \
-      --font "${themeFonts.sans}" \
+      --bs-hl-color ffffffff \
       --clock \
-      --indicator \
-      --indicator-radius 120 \
-      --indicator-thickness 7 \
+      --daemonize \
       --effect-blur 15x8 \
       --effect-vignette 0.3:0.3 \
       --fade-in 1 \
-      --ring-color d0d0d0 \
-      --ring-ver-color ffffff \
-      --ring-wrong-color ff3b30 \
-      --ring-clear-color 34c759 \
+      --font "${themeFonts.sans}" \
+      --ignore-empty-password \
+      --image /home/nathaniel/Pictures/.background-image.jpg \
+      --indicator \
+      --indicator-caps-lock \
+      --indicator-idle-visible \
+      --indicator-radius 120 \
+      --indicator-thickness 7 \
+      --inside-clear-color 34c75940 \
       --inside-color 00000040 \
       --inside-ver-color 00000020 \
       --inside-wrong-color ff3b3040 \
-      --inside-clear-color 34c75940 \
-      --separator-color 00000000 \
-      --line-color 00000000 \
-      --line-clear-color 00000000 \
-      --line-wrong-color 00000000 \
       --key-hl-color ffffffff \
-      --bs-hl-color ffffffff \
-      --text-color ffffff \
+      --line-clear-color 00000000 \
+      --line-color 00000000 \
+      --line-wrong-color 00000000 \
+      --ring-clear-color 34c759 \
+      --ring-color d0d0d0 \
+      --ring-ver-color ffffff \
+      --ring-wrong-color ff3b30 \
+      --separator-color 00000000 \
+      --show-failed-attempts \
       --text-clear-color ffffff \
+      --text-color ffffff \
       --text-ver-color ffffff \
-      --text-wrong-color ffffff
+      --text-wrong-color ffffff      
   '';
 }
