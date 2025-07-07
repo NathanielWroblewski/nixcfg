bac8b1e95684a10a2de6a104ab8e4312eeb24d62
NathanielWroblewski <nathanielwroblewski@gmail.com>
2025-07-06 22:04:52 -0400

    Add wezterm configuration

--- pkgcfg/user-level/wezterm.nix
+++ pkgcfg/user-level/wezterm.nix
@@ -16,7 +16,27 @@ in
         font = wezterm.font('${fonts.mono}'),
         window_background_opacity = 0.85,
 
-        hide_tab_bar_if_only_one_tab = true
+        adjust_window_size_when_changing_font_size = false,
+        enable_scroll_bar = true,
+        enable_wayland = true,
+        hide_tab_bar_if_only_one_tab = true,
+        initial_cols = 100,
+        kde_window_background_blur = true,
+        mouse_wheel_scrolls_tabs = false,
+        scrollback_lines = 10000,
+
+        keys = {
+          {
+            key = 'c',
+            mods = 'CMD',
+            action = wezterm.action.CopyTo 'Clipboard',
+          },
+          {
+            key = 'v',
+            mods = 'CMD',
+            action = wezterm.action.PasteFrom 'Clipboard',
+          },
+        },
       }
     '';
   };
