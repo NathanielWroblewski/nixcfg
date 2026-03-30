5552abd1a1a6d516c2a9c319006edb4ea3d7cc71
NathanielWroblewski <nathanielwroblewski@gmail.com>
2026-03-30 16:18:37 -0400

    Migrate xdg portals configuration from user-level to system-level

--- pkgcfg/system-level/niri.nix
+++ pkgcfg/system-level/niri.nix
@@ -1,3 +1,19 @@
+{ pkgs, ... }:
 {
   programs.niri.enable = true;
+
+  # enable screensharing
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+
+    config.common.default = [
+      "gnome"
+      "gtk"
+    ];
+  };
+
 }
--- pkgcfg/user-level/niri.nix
+++ pkgcfg/user-level/niri.nix
@@ -1,18 +1,4 @@
-{ pkgs, ... }:
 {
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-    ];
-
-    config.common.default = [
-      "gnome"
-      "gtk"
-    ];
-  };
-
   home.file = {
     ".config/niri/config.kdl".source = ../../dotfiles/niri-config.kdl;
   };
